ea4191551ef2639ac6cf4f5cedacaabdc4d33c4c
jerin77777 <jerinjacob0007@gmail.com>
2024-01-20 22:23:57 +0530

    backup

--- history_be/functions/utils.py
+++ history_be/functions/utils.py
@@ -3,8 +3,7 @@ import os
 from flask_socketio import emit
 import json
 
-keys = ["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", "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"]
-key = keys[1]
+key = "your_key"
 os.environ["OPENAI_API_KEY"] = key
 
 client = OpenAI(
